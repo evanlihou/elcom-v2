1c08f6b1427f02553d33663ab46d889e0bfb66cc
Evan Lihou <evanlihou@evanlihou.com>
2019-06-22 15:11:40 -0400

    Visual bug fixes for dark mode

--- src/Common/Loading.jsx
+++ src/Common/Loading.jsx
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
+import './Loading.sass'
 import ReactLoading from 'react-loading';
 
 class Loading extends Component {
   render() {
     return (
-      <ReactLoading type="cubes" color="#000000" style={loadingStyle} />
+      <ReactLoading color={null} type="cubes" className="loadingIndicator"/>
     )
   }
-}
-
-const loadingStyle = {
-  margin: '0 auto',
-  width: '50px'
 }
 
 export default Loading;
