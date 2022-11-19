6bc71e442bc5e0b5f7f0fd862e9ff091fc1e9779
Evan Lihou <e@evandoes.dev>
2022-11-19 17:48:02 -0500

    Properly sort resume entries

--- src/app/MainPage/Resume.jsx
+++ src/app/MainPage/Resume.jsx
@@ -6,7 +6,7 @@ import ReactMarkdown from 'react-markdown';
 const intlOptions = new Intl.DateTimeFormat('en-US', {month: 'long', year: 'numeric', timeZone: 'UTC'})
 
 export default async function Resume() {
-  const {data} = await fetchAPI('/resume-entries');
+  const {data} = await fetchAPI('/resume-entries', {sort: 'Start:desc'});
     return (
       <div>
         <h3>Curriculum Vitae</h3>
